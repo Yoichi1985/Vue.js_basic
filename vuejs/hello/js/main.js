b296f24c75ac57029f9a7a32b933eaa2a4a61700
Yoichi1985 <tasaka4001@gmail.com>
2020-01-05 11:02:42 +0900

    Vue.js_basic

---
+++ vuejs/hello/js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+{
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello World',
+      url: 'https://jp.vuejs.org/',
+      toggle: true,
+      languages: [
+        'Javascript',
+        'Ruby',
+        'Python'
+      ]
+    },
+    methods: {
+      clickHandler: function () {
+        this.message = 'Clicked!'
+      }
+    }
+  })
+}
